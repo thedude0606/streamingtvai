072efc70167a9f4203663d67907473140986b751
thedude0606 <thedude0606@example.com>
2025-07-22 12:53:14 -0400

    fix: Correct SyntaxError in backend/src/main.py by removing unnecessary escape characters

--- backend/src/main.py
+++ backend/src/main.py
@@ -11,8 +11,8 @@ from src.routes.campaign import campaign_bp
 
 app = Flask(__name__, static_folder=os.path.join(os.path.dirname(__file__), 'static'))
 app.config['SECRET_KEY'] = 'asdf#FGSgvasgf$5$WGT'
-app.register_blueprint(user_bp, url_prefix=\"/api\")
-app.register_blueprint(campaign_bp, url_prefix=\"/api\")
+app.register_blueprint(user_bp, url_prefix="/api")
+app.register_blueprint(campaign_bp, url_prefix="/api")
 # uncomment if you need to use database
 app.config['SQLALCHEMY_DATABASE_URI'] = f"sqlite:///{os.path.join(os.path.dirname(__file__), 'database', 'app.db')}"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
